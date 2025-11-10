c6909de2f4086208b4e3fd393fae5c48f2356b26
mohitrajora <mohitrajora1432@gmail.com>
2025-11-11 02:04:40 +0530

    update slug

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBlog, getBlogs, deleteBlog, updateBlog, getSingleBlog } from "../controller/blogController.js";
+import { addBlog, getBlogs, deleteBlog, updateBlog, getSingleBlog, getBlogBySlug } from "../controller/blogController.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
